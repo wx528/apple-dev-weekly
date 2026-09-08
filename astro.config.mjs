2cde951efa15d1ad2a04e266d4a9bd0dd440fcc7
wx528 <33819629+wx528@users.noreply.github.com>
2026-09-08 22:44:21 +0800

    chore: update site URL to workers.dev domain

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,6 +2,6 @@ import { defineConfig } from 'astro/config';
 import sitemap from '@astrojs/sitemap';
 
 export default defineConfig({
-  site: 'https://apple-dev-weekly.pages.dev',
+  site: 'https://apple-dev-weekly.muchjs.workers.dev',
   integrations: [sitemap()],
 });
