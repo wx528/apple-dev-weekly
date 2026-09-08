be18f00b41a34c4547c5636c7592fd7db375ad0d
wx528 <33819629+wx528@users.noreply.github.com>
2026-09-08 16:18:06 +0800

    chore: scaffold Astro site

---
+++ astro.config.mjs
@@ -0,0 +1,7 @@
+import { defineConfig } from 'astro/config';
+import sitemap from '@astrojs/sitemap';
+
+export default defineConfig({
+  site: 'https://apple-dev-weekly.pages.dev',
+  integrations: [sitemap()],
+});
